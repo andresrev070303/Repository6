00acb6bcefe9b3beeb53435cc74f719dfddad3f3
Andres Revollo <andres.revollo@usb.edu.bo>
2024-03-18 23:00:38 -0400

    Se refactorizo el resto del codigo

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,12 +1,22 @@
 class Juego {
   constructor() {
     this.score = 0;
+    this.rolls = [];
+    this.currentRoll = 0;
   }
 
   roll(pins) {
     this.score += pins;
+    this.rolls[this.currentRoll++] = pins;
   }
   getScore() {
+    this.score = 0;
+    for (let i = 0; i < this.rolls.length; i++) {
+      if(this.rolls[i] + this.rolls[i + 1] === 10) {
+        this.score += this.rolls[i];
+      }
+      this.score += this.rolls[i];
+    }
     return this.score;
   }
 }
