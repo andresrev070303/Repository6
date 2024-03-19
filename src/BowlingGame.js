9e4425c4174fe5a11350c2ff844b93dc38738bda
Andres Revollo <andres.revollo@usb.edu.bo>
2024-03-18 23:14:42 -0400

    Se agrego la funcionalidad de un spare

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,23 +1,26 @@
 class Juego {
   constructor() {
-    this.score = 0;
-    this.rolls = [];
+    this.rolls = new Array(21).fill(0);
     this.currentRoll = 0;
   }
 
   roll(pins) {
-    this.score += pins;
     this.rolls[this.currentRoll++] = pins;
   }
-  getScore() {
-    this.score = 0;
-    for (let i = 0; i < this.rolls.length; i++) {
-      if(this.rolls[i] + this.rolls[i + 1] === 10) {
-        this.score += this.rolls[i];
+  score() {
+    let score = 0;
+    let i = 0;
+    for (let frame = 0; frame < 10; frame++) {
+      if (this.rolls[i] + this.rolls[i + 1] === 10) {
+        score += 10 + this.rolls[i + 2];
+        i += 2;
+      } else {
+        score += this.rolls[i] + this.rolls[i + 1];
+        i += 2;
       }
-      this.score += this.rolls[i];
     }
-    return this.score;
+    return score;
   }
+
 }
 export default Juego;
--- src/BowlingGame.test.js
+++ src/BowlingGame.test.js
@@ -12,10 +12,17 @@ describe("Jugar", () => {
   }
   it("deberia devolver 0 de la funcion juego", () => {
     rollMany(20, 0);
-    expect(juego.getScore()).toBe(0);
+    expect(juego.score()).toBe(0);
   });
   it("deberia devolver 20 de la funcion juego", () => {
     rollMany(20, 1);
-    expect(juego.getScore()).toBe(20);
+    expect(juego.score()).toBe(20);
   });
-});
+  it("deberia devolver el caso especial de 1 spare", () => {
+    juego.roll(5);
+    juego.roll(5);
+    juego.roll(3);
+    rollMany(17, 0);
+    expect(juego.score()).toBe(16);
+  });
+}); 
